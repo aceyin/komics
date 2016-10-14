5dcbc4676997fdbd4e263dfd448b274efa3c0bec
aceyin <ync@163.com>
2016-10-14 18:09:45 +0800

    adjust module conf

--- komics-core/src/main/kotlin/komics/util/Utils.kt
+++ komics-core/src/main/kotlin/komics/util/Utils.kt
@@ -1,9 +1,5 @@
 package komics.util
 
-import komics.data.Entity
-import javax.persistence.Table
-import kotlin.reflect.KClass
-
 object StrUtil {
     /**
      * 判断给定的字符串是否包含空。
@@ -25,24 +21,5 @@ object StrUtil {
         if (values == null) return false
         values.forEach { if (it.isNullOrEmpty()) return false }
         return true
-    }
-}
-
-/**
- * Entity util
- */
-object EntityUtil {
-    /**
-     * build a sql with an entity class as the table name
-     */
-    fun <T : Entity> table(c: KClass<T>): String {
-        val cname = c.simpleName ?: throw RuntimeException("Name of entity class '$c' cannot be null")
-        val t = c.annotations.find { it.annotationClass == Table::class } as? Table
-
-        t?.let {
-            if (t.name.isNullOrEmpty()) return cname
-            return t.name
-        }
-        return cname
     }
 }
